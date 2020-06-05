34da1f6ff01640f0d135bcccc668084c6d8042b9
osagie obaretin <osagie.codes@gmail.com>
2020-06-05 06:33:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,3 +27,12 @@ end
 line(katz_deli)
 
 
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
+
+now_serving(katz_deli)
